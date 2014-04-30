32800a47a7d90c21cdfe4f5da33a4b9d1d4be86d
Eric Larchevêque <elarch@gmail.com>
2014-04-30 18:52:35 +0200

    Devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def flash_class(level)
+    case level
+      when :notice then "note note-info"
+      when :success then "note note-success"
+      when :error then "note note-danger"
+      when :alert then "note note-warning"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430092012) do
+ActiveRecord::Schema.define(version: 20140430142446) do
 
   create_table "accounts", force: true do |t|
     t.integer  "coin_id",               null: false
@@ -67,17 +67,17 @@ ActiveRecord::Schema.define(version: 20140430092012) do
   add_index "payouts", ["transaction_id"], name: "index_payouts_on_transaction_id"
 
   create_table "shares", force: true do |t|
-    t.integer  "worker_id",                                       null: false
-    t.string   "solution",    limit: 64,                          null: false
-    t.float    "difficulty",                                      null: false
-    t.boolean  "our_result",                                      null: false
+    t.integer  "worker_id",              null: false
+    t.string   "pool",        limit: 64, null: false
+    t.string   "solution",    limit: 64, null: false
+    t.float    "difficulty",             null: false
+    t.boolean  "our_result",             null: false
     t.boolean  "pool_result"
     t.string   "reason"
-    t.boolean  "is_block",                                        null: false
+    t.boolean  "is_block",               null: false
+    t.integer  "payout_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "payout_id"
-    t.string   "pool",        limit: 64, default: "CleverMining", null: false
   end
 
   add_index "shares", ["payout_id"], name: "index_shares_on_payout_id"
@@ -92,18 +92,35 @@ ActiveRecord::Schema.define(version: 20140430092012) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "name",           limit: 64
-    t.string   "password",       limit: 64
+    t.string   "name",                   limit: 64
+    t.string   "password",               limit: 64
     t.string   "email"
-    t.string   "payout_address", limit: 34,                                          null: false
-    t.boolean  "is_anonymous",                                                       null: false
-    t.boolean  "is_admin",                                                           null: false
+    t.string   "payout_address",         limit: 34,                                       null: false
+    t.boolean  "is_anonymous",                                                            null: false
+    t.boolean  "is_admin",                                                                null: false
+    t.integer  "balance",                                                    default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "balance",                                            default: 0
-    t.string   "wallet_address", limit: 34
-    t.decimal  "min_price",                 precision: 16, scale: 8, default: 0.001, null: false
+    t.string   "wallet_address",         limit: 34
+    t.decimal  "min_price",                         precision: 16, scale: 8
+    t.string   "encrypted_password",                                         default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",                                              default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
+
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email"
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "workers", force: true do |t|
     t.integer  "user_id",                 null: false

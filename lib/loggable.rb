b9be2eaed2f5d7df6658a6c9951797dfb76ee321
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2014-05-07 15:17:04 +0200

    [Loggable] refactor

--- lib/loggable.rb
+++ lib/loggable.rb
@@ -18,16 +18,23 @@ module Loggable
   }
 
   def Loggable.included( aMod )
-    id = aMod.name.underscore
+    id = identify( aMod )
+    logger = logger( id )
+    aMod.instance_variable_set :@log, logger
+    aMod.extend( LogMethods )
+  end
+
+  def Loggable.logger( id )
+    logger = Log4r::Logger[id]
+    return logger if logger
+
     logger = Log4r::Logger.new( id )
     log_filename = "./log/loggers/" + id + ".log"
     `mkdir -p #{File.dirname(log_filename)}`
     file_outputter = Log4r::Outputter["#{id}_file"] || Log4r::FileOutputter.new( "#{id}_file", level: Log4r::DEBUG, filename: log_filename, formatter: PATTERN )
     stdout_outputter = Log4r::Outputter["our_stdout"] || Log4r::ColorOutputter.new( "our_stdout", level: DEFAULT_STDOUT_LEVEL, formatter: PATTERN, colors: COLORS)
     logger.outputters = [stdout_outputter, file_outputter]
-
-    aMod.instance_variable_set :@log, logger
-    aMod.extend( LogMethods )
+    logger
   end
 
   # Used is test/test_helper
@@ -40,6 +47,18 @@ module Loggable
   def Loggable.unmute
     Log4r::Logger.global.level = Log4r::ALL
     Log4r::Logger.each_logger { |logger| logger.level = Log4r::ALL }
+  end
+
+  def Loggable.log( str )
+    self.logger( 'default' ).info( str )
+  end
+
+  def Loggable.identify( obj )
+    if obj.kind_of?( Module )
+      obj.name.underscore
+    else
+      obj.class.name.underscore
+    end
   end
 
   def log

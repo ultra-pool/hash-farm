ec7d7e8ba765e221e8609ae2df39dde8e9f8a049
Vincent Renaudineau <vincent.renaudineau@prixing.fr>
2014-05-12 11:53:49 +0200

    [Transfer] add scopes and helpers

--- app/models/transfer.rb
+++ app/models/transfer.rb
@@ -1,6 +1,10 @@
+# -*- encoding : utf-8 -*-
+
 class Transfer < ActiveRecord::Base
   belongs_to :sender, :class_name => "User"
   belongs_to :recipient, :class_name => "User"
+
+  # VALIDATION
 
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 10**-8, less_than: 21 * 10**6 }
   validates :txid, length: { is: 64 }, format: { with: /\A[a-fA-F0-9]+\z/ }, allow_nil: true
@@ -9,14 +13,35 @@ class Transfer < ActiveRecord::Base
   validates :vout, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
 
   def assert_2_and_only_2_of_sender_recipient_and_txid_are_present
-    if sender.present?
-      errors[:base] << "sender is present, wait one and only one of recipient and txid to present" unless recipient.present? ^ txid.present?
+    if sender_id.present?
+      errors[:base] << "sender is present, wait one and only one of recipient and txid to be present" unless recipient_id.present? ^ txid.present?
     else
-      errors[:base] << "sender is not present, wait recipient and txid to be present" unless recipient.present? && txid.present?
+      errors[:base] << "sender is not present, wait recipient and txid to be present" unless recipient_id.present? && txid.present?
     end
   end
 
   def assert_vout_present_if_and_only_if_txid_is_present
     errors.add(:vout, "vout must be present with and only with txid") if vout.present? != txid.present?
+  end
+
+  # SCOPES
+
+  scope :from, -> (user) { where( sender: user ) }
+  scope :to, -> (user) { where( recipient: user ) }
+  scope :of,  -> (user) { where( "sender_id = ? OR recipient_id = ?", user, user ) }
+
+  def self.balance( user )
+    to( user ).pluck(:amount).sum - from( user ).pluck(:amount).sum
+  end
+
+  def self.payouts( user )
+    from( user ).where( recipient_id: nil )
+  end
+  def self.withdraws( user )
+    self.payouts( user )
+  end
+
+  def self.credits( user )
+    to( user ).where( sender_id: nil )
   end
 end
--- test/models/transfer_test.rb
+++ test/models/transfer_test.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'test_helper'
 
 class TransferTest < ActiveSupport::TestCase
@@ -5,18 +6,18 @@ class TransferTest < ActiveSupport::TestCase
     @txid = "d1e21e283d5e917b2ddd01c0a579d41d4b0e60062890f1958485ea86b3f24743"
     @vout = 0
     @barbu = users(:barbu)
-    @mv = users(:mv)
+    @toto = users(:toto)
     @amount = 0.42
-    @transfer = Transfer.new( sender: @barbu, recipient: @mv, amount: @amount )
+    @transfer = Transfer.new( sender: @barbu, recipient: @toto, amount: @amount )
   end
 
   test "it should assert amount is present" do
-    assert Transfer.new( sender: @barbu, recipient: @mv, amount: @amount ).valid?
-    assert Transfer.new( sender: @barbu, recipient: @mv, amount: 0 ).invalid?
-    assert Transfer.new( sender: @barbu, recipient: @mv, amount: 10**9 ).invalid?
-    assert Transfer.new( sender: @barbu, recipient: @mv, amount: nil ).invalid?
-    assert Transfer.new( sender: @barbu, recipient: @mv, amount: "toto" ).invalid?
-    assert Transfer.new( sender: @barbu, recipient: @mv ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, amount: @amount ).valid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, amount: 0 ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, amount: 10**9 ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, amount: nil ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, amount: "toto" ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto ).invalid?
   end
 
   test "it should check txid format" do
@@ -33,17 +34,55 @@ class TransferTest < ActiveSupport::TestCase
     assert Transfer.new( sender: @barbu, txid: @txid, vout: 1.2, amount: @amount ).invalid?
     assert Transfer.new( sender: @barbu, txid: @txid, vout: -1, amount: @amount ).invalid?
     assert Transfer.new( sender: @barbu, txid: @txid, amount: @amount ).invalid?
-    assert Transfer.new( sender: @barbu, recipient: @mv, vout: @vout, amount: @amount ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, vout: @vout, amount: @amount ).invalid?
   end
 
   test "it should assert_2_and_only_2_of_sender_recipient_and_txid_are_present" do
-    assert Transfer.new( sender: @barbu, recipient: @mv, amount: @amount ).valid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, amount: @amount ).valid?
     assert Transfer.new( sender: @barbu, txid: @txid, vout: @vout, amount: @amount ).valid?
     assert Transfer.new( recipient: @barbu, txid: @txid, vout: @vout, amount: @amount ).valid?
 
     assert Transfer.new( sender: @barbu, amount: @amount ).invalid?
-    assert Transfer.new( recipient: @mv, amount: @amount ).invalid?
+    assert Transfer.new( recipient: @toto, amount: @amount ).invalid?
     assert Transfer.new( txid: @txid, vout: @vout, amount: @amount ).invalid?
-    assert Transfer.new( sender: @barbu, recipient: @mv, txid: @txid, vout: @vout, amount: @amount ).invalid?
+    assert Transfer.new( sender: @barbu, recipient: @toto, txid: @txid, vout: @vout, amount: @amount ).invalid?
+  end
+
+  test "it should scope from" do
+    tfs = Transfer.from( @barbu )
+    # assert Transfer.all.all? { |tf| tf.valid? || puts( tf.errors.inspect ) }
+    assert_equal 2, tfs.size
+    assert tfs.all? { |tf| tf.sender == @barbu }
+  end
+
+  test "it should scope to" do
+    tfs = Transfer.to( @toto )
+    assert_equal 2, tfs.size
+    assert tfs.all? { |tf| tf.recipient == @toto }, tfs.map(&:inspect).join("\n")
+  end
+
+  test "it should scope of" do
+    tfs = Transfer.of( @barbu )
+    assert_equal 3, tfs.size
+    assert tfs.all? { |tf| tf.sender == @barbu || tf.recipient == @barbu }
+
+    tfs = Transfer.of( @toto )
+    assert_equal 3, tfs.size
+    assert tfs.all? { |tf| tf.sender == @toto || tf.recipient == @toto }
+  end
+
+  test "it should compute balance" do
+    assert_equal 0, Transfer.balance( @barbu )
+    assert_equal 0.2, Transfer.balance( @toto )
+  end
+
+  test "it should get credits" do
+    assert_equal 1, Transfer.credits( @barbu ).size
+    assert_equal 0, Transfer.credits( @toto ).size
+  end
+
+  test "it should get payouts" do
+    assert_equal 0, Transfer.payouts( @barbu ).size
+    assert_equal 1, Transfer.payouts( @toto ).size
   end
 end
